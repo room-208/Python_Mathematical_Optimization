5aae51aad33a3cee7b356f0f81e47708980ef9ed
room-208 <yumalegomindstorm@gmail.com>
2022-04-03 20:49:36 +0900

    3章を読みました

--- 3.school/school.py
+++ 3.school/school.py
@@ -1,3 +1,4 @@
+from unittest import result
 import pulp
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,20 +7,37 @@ import os
 DIRNAME = "./3.school"
 
 
+def get_init_flag(student_df, S, C):
+    df = student_df.copy()
+    df = df.sort_values(by='score', ascending=False)
+    class_dict = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H"}
+    df["init_assigned_class"] = -1
+    for i in range(len(df)):
+        df.loc[i, "init_assigned_class"] = class_dict[i % 8]
+    init_flag = {(s, c): 0 for s in S for c in C}
+    for _, row in df.iterrows():
+        init_flag[row["student_id"], row["init_assigned_class"]] = 1
+    return init_flag
+
+
 def solve():
 
     student_df = pd.read_csv(os.path.join(DIRNAME, "students.csv"))
     student_pair_df = pd.read_csv(os.path.join(DIRNAME, "student_pairs.csv"))
 
-    problem = pulp.LpProblem("ClassAssignmentProblem", pulp.LpMaximize)
-
     S = student_df["student_id"].to_list()
     C = ["A", "B", "C", "D", "E", "F", "G", "H"]
+
+    init_flag = get_init_flag(student_df, S, C)
+
+    problem = pulp.LpProblem("ClassAssignmentProblem", pulp.LpMaximize)
 
     x = {}
     for s in S:
         for c in C:
             x[s, c] = pulp.LpVariable(f"x_{s,c}", cat='Binary')
+
+    problem += pulp.lpSum([x[s, c] for s in S for c in C if init_flag[s, c] == 1])
 
     for s in S:
         problem += pulp.lpSum([x[s, c] for c in C]) == 1
@@ -66,6 +84,36 @@ def solve():
         print(f"Num: {len(Ss)}")
         print(f"Student: {Ss}")
         print()
+
+    for s in S:
+        assigned_class = [x[s, c].value() for c in C if x[s, c].value() == 1]
+        assert len(assigned_class) == 1, f"error: {s, assigned_class}"
+
+    S2C = {s: c for s in S for c in C if x[s, c].value() == 1}
+
+    result_df = student_df.copy()
+    result_df["assigned_class"] = result_df["student_id"].map(S2C)
+    print(result_df.groupby(["assigned_class"])["student_id"].count())
+    print(result_df.groupby(["assigned_class", "gender"])["student_id"].count())
+    print(result_df.groupby(["assigned_class"])["score"].mean())
+    print(result_df.groupby(["assigned_class"])["leader_flag"].sum())
+    print(result_df.groupby(["assigned_class"])["support_flag"].sum())
+
+    SS = [(row["student_id1"], row["student_id2"]) for _, row in student_pair_df.iterrows()]
+    for s1, s2 in SS:
+        c1 = S2C[s1]
+        c2 = S2C[s2]
+
+        assert c1 != c2, "c1 == c2"
+        print(f"s1 = {s1}, c1 = {c1}")
+        print(f"s2 = {s2}, c2 = {c2}")
+
+    fig = plt.figure(figsize=(12, 20))
+    for i, c in enumerate(C):
+        cls_df = result_df[result_df["assigned_class"] == c]
+        ax = fig.add_subplot(4, 2, i+1, xlabel="score", ylabel="num", xlim=(0, 500), ylim=(0, 20), title=f"Class:{c}")
+        ax.hist(cls_df["score"], bins=range(0, 500, 40))
+    plt.show()
 
 
 if __name__ == "__main__":
