512218e4617e2fe02b7b36a0773d7f680ef69925
room-208 <yumalegomindstorm@gmail.com>
2022-04-03 04:27:27 +0000

    2章のチュートリアルを実施しています

--- 2.tutorial/tutorial.py
+++ 2.tutorial/tutorial.py
@@ -1,7 +1,23 @@
 import pulp
 
-def main():
-    print("a")
+
+def solve():
+
+    problem = pulp.LpProblem("SLE", pulp.LpMaximize)
+
+    x = pulp.LpVariable("x", cat='Continuous')
+    y = pulp.LpVariable("y", cat='Continuous')
+
+    #problem += 120*x + 150 * y == 1440
+    #problem += x+y == 10
+    problem.addConstraint(120*x + 150 * y == 1440)
+    problem.addConstraint(x+y == 10)
+
+    status = problem.solve()
+
+    print(f"Status:{pulp.LpStatus[status]}")
+    print(f"x={x.value()}, y={y.value()},")
+
 
 if __name__ == "__main__":
-    main()
+    solve()
