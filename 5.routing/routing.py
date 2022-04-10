aaeacc0949bf38ea45f647faca1b7cb54783597d
room-208 <yumalegomindstorm@gmail.com>
2022-04-10 11:01:54 +0900

    ファイル名を更新しました。

---
+++ 5.routing/routing.py
@@ -0,0 +1,19 @@
+from cmath import cos, pi
+from turtle import pu
+import pulp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import time
+
+DIRNAME = "./5.routing"
+
+
+class RouteSolver:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    solver = RouteSolver()
