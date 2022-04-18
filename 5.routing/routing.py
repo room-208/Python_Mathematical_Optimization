808b7e33960dc0b9973ab318bccbe57c7d400909
room-208 <yumalegomindstorm@gmail.com>
2022-04-18 18:11:50 +0900

    デバッグ中です

--- 5.routing/routing.py
+++ 5.routing/routing.py
@@ -1,5 +1,7 @@
 from turtle import pu
+from typing import overload
 from IPython.core.display import display
+from flask import current_app
 import numpy as np
 import pandas as pd
 import os
@@ -32,17 +34,19 @@ class RouteSolver:
         self.K = range(self.num_places)
         self.o = 0
         self.K_minus_0 = self.K[1:]
-        self._K = np.random.normal(0, self.mean_travel_time_to_destinations, size=(len(self.K), 2))
+        self._K = np.random.normal(
+            0, self.mean_travel_time_to_destinations, size=(len(self.K), 2))
         self._K[self.o, :] = 0
-        self.t = np.array([[np.floor(np.linalg.norm(self._K[k]-self._K[l]))for k in self.K]for l in self.K])
+        self.t = np.array(
+            [[np.floor(np.linalg.norm(self._K[k]-self._K[l]))for k in self.K]for l in self.K])
         self.D = range(self.num_days)
 
         self.R = range(self.num_requests)
         self.k = np.random.choice(self.K_minus_0, size=len(self.R))
-        print(self.k)
         self.d_0 = np.random.choice(self.D, size=len(self.R))
         self.d_1 = self.d_0 + self.delivery_time_window-1
-        self.w = np.floor(np.random.gamma(10, self.avg_weight/10, size=len(self.R)))
+        self.w = np.floor(np.random.gamma(
+            10, self.avg_weight/10, size=len(self.R)))
         self.f = np.ceil(self.w/100)*self.delivery_outsourcing_unit_cost
 
         self.routes_df = None
@@ -85,7 +89,8 @@ class RouteSolver:
         for k, l in product(self.K_minus_0, self.K_minus_0):
             daily_route_prob += u[k] + 1 <= u[l] + (len(self.K)-1)*(1-x[k, l])
 
-        travel = pulp.lpSum([self.t[k, l]*x[k, l] for k, l in product(self.K, self.K)])
+        travel = pulp.lpSum([self.t[k, l]*x[k, l]
+                            for k, l in product(self.K, self.K)])
         daily_route_prob += (travel-self.H_regular) <= h
         daily_route_prob += h <= self.H_max_overtime
 
@@ -101,7 +106,8 @@ class RouteSolver:
 
     def enumerate_routes(self):
         routes = Parallel(n_jobs=16)(
-            [delayed(self.simulate_route)(z) for z in product([0, 1], repeat=len(self.K))]
+            [delayed(self.simulate_route)(z)
+             for z in product([0, 1], repeat=len(self.K))]
         )
         routes = pd.DataFrame(filter(lambda x: x is not None, routes))
         self.routes_df = routes[routes.optimal].copy()
@@ -114,7 +120,8 @@ class RouteSolver:
         best_route_idx = None
         best_hours = sys.float_info.max
         for route_idx, row in self.routes_df.iterrows():
-            all_requests_on_route = all([row.z[self.k[r]] == 1 for r in requests])
+            all_requests_on_route = all(
+                [row.z[self.k[r]] == 1 for r in requests])
             if all_requests_on_route and row.移動時間 < best_hours:
                 best_route_idx = route_idx
                 best_hours = row.移動時間
@@ -123,8 +130,70 @@ class RouteSolver:
         else:
             return best_route_idx, best_hours
 
+    def _enumerate_feasible_schedules(self, requests_cands, current_idx_set, idx_to_add, res):
+        idx_set_to_check = current_idx_set + [idx_to_add]
+        next_idx = idx_to_add + 1
+        is_next_idx_valid = next_idx < len(requests_cands)
+        requests = [requests_cands[i] for i in idx_set_to_check]
+        is_ok = self.is_OK(requests)
+
+        if is_ok:
+            best_route_idx, best_hours = is_ok
+            res.append({'requests': [requests_cands[i]
+                                     for i in idx_set_to_check],
+                        'route_idx': best_route_idx,
+                        'hours': best_hours})
+
+            if is_next_idx_valid:
+                self._enumerate_feasible_schedules(
+                    requests_cands, idx_set_to_check, next_idx, res)
+
+        if is_next_idx_valid:
+            self._enumerate_feasible_schedules(
+                requests_cands, current_idx_set, next_idx, res)
+
+    def enumerate_feasible_schedules(self, d):
+        requests_cands = [r for r in self.R if self.d_0[r] <= d <= self.d_1[r]]
+
+        res = [{'requests': [],
+                'route_idx': 0,
+                'hours': 0}]
+
+        self._enumerate_feasible_schedules(requests_cands, [], 0, res)
+
+        feasible_schedules_df = pd.DataFrame(res)
+        feasible_schedules_df["overwork"] = (
+            feasible_schedules_df.hours-self.H_regular).clip(0)
+        feasible_schedules_df["requests_set"] = feasible_schedules_df.requests.apply(
+            set)
+
+        idx_cands = set(feasible_schedules_df.index)
+        dominated_idx_set = set()
+        for dominant_idx in feasible_schedules_df.index:
+            for checked_idx in feasible_schedules_df.index:
+                requests_strict_dominance = (
+                    feasible_schedules_df.requests_set.loc[checked_idx] <
+                    feasible_schedules_df.requests_set.loc[dominated_idx_set]
+                )
+                overwork_weak_dominance = (
+                    feasible_schedules_df.overwork.loc[checked_idx] >=
+                    feasible_schedules_df.overwork.loc[dominated_idx_set]
+                )
+                if requests_strict_dominance and overwork_weak_dominance:
+                    dominated_idx_set.add(checked_idx)
+
+        nondominated_idx = idx_cands - dominated_idx_set
+        nondominated_feasible_schedules_df = feasible_schedules_df.loc[nondominated_idx, :]
+        return nondominated_feasible_schedules_df
+
 
 if __name__ == "__main__":
     solver = RouteSolver()
     # solver.plot_everything()
-    print(solver.enumerate_routes())
+    # print(solver.enumerate_routes())
+    print(solver.D)
+    print(solver.R)
+
+    # _schedules = Parallel(n_jobs=16)(
+    #    [delayed(solver.enumerate_feasible_schedules)(d) for d in solver.D])
+    #feasible_schedules = dict(zip(solver.D, _schedules))
